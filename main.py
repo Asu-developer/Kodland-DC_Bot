3c3bb87a307f8f73f5e5c1e5e6b63f068ec95a97
Adnan Selim <adnanuguzselim@gmail.com>
2026-05-03 13:10:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("heh" * count_heh)
+
+@bot.command()
+async def Toplama(ctx, sayi_1, sayi_2):
+    await ctx.send(int(sayi_1) + int(sayi_2))
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+
+bot.run("Token")
